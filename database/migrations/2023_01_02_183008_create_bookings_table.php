2edeaf663b86252ba741fae21f333d266bc5ebeb
Juan Camilo Rosillo Martinez <juanca1158@hotmail.com>
2023-01-02 13:37:21 -0500

    [Feat]Create migrations

---
+++ database/migrations/2023_01_02_183008_create_bookings_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('activity_id');
+            $table->integer('number_people');
+            $table->decimal('price');
+            $table->dateTime('date');
+            $table->dateTime('activity_date');
+            $table->timestamps();
+
+            $table->foreign('activity_id')
+                ->references('id')->on('activities')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bookings');
+    }
+};
